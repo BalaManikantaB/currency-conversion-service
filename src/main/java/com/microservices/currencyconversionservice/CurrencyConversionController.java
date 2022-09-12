b5d3e5e77b64d5156db560db089ba648c61c0b8f
Bala Manikanta Bonthu <balamanikantabonthu@Balas-MacBook-Pro.local>
2022-09-12 16:56:13 +0530

    Add ability to register with naming server and to load balance between client instances

--- src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -14,38 +14,37 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
-    @Autowired
-    Environment environment;
-    @Autowired
-    CurrencyExchangeProxy proxy;
+        @Autowired
+        Environment environment;
+        @Autowired
+        CurrencyExchangeProxy proxy;
 
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
-            @PathVariable BigDecimal quantity) {
-        HashMap<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
+        @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+        public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
+                        @PathVariable BigDecimal quantity) {
+                HashMap<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("from", from);
+                uriVariables.put("to", to);
 
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
-                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                CurrencyConversion.class, uriVariables);
+                ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+                                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                                CurrencyConversion.class, uriVariables);
 
-        String port = environment.getProperty("local.server.port");
+                String port = environment.getProperty("local.server.port");
 
-        CurrencyConversion currencyConversion = responseEntity.getBody();
-        return new CurrencyConversion(1000L, from, to, quantity, currencyConversion.getConversionMultiple(),
-                currencyConversion.getConversionMultiple().multiply(quantity), port);
-    }
+                CurrencyConversion currencyConversion = responseEntity.getBody();
+                return new CurrencyConversion(1000L, from, to, quantity, currencyConversion.getConversionMultiple(),
+                                currencyConversion.getConversionMultiple().multiply(quantity), port);
+        }
 
-    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyFeignConversion(@PathVariable String from, @PathVariable String to,
-            @PathVariable BigDecimal quantity) {
+        @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+        public CurrencyConversion calculateCurrencyFeignConversion(@PathVariable String from, @PathVariable String to,
+                        @PathVariable BigDecimal quantity) {
 
-        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+                CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 
-        String port = environment.getProperty("local.server.port");
-
-        return new CurrencyConversion(1000L, from, to, quantity, currencyConversion.getConversionMultiple(),
-                currencyConversion.getConversionMultiple().multiply(quantity), port);
-    }
+                return new CurrencyConversion(1000L, from, to, quantity, currencyConversion.getConversionMultiple(),
+                                currencyConversion.getConversionMultiple().multiply(quantity),
+                                currencyConversion.getEnvironment());
+        }
 }
